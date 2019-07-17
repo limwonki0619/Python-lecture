48276e389905b46fd375da4a8588d9de116a25a0
Wonki lim <limwk0619@naver.com>
2019-07-17 22:56:32 +0900

    예제문제 수정

--- Python_coding_dojang_test/python_example_01.py
+++ Python_coding_dojang_test/python_example_01.py
@@ -41,27 +41,29 @@ print(1900 % 400 == 0)  # F 400의 배수가 아니다. -> 평년
 # 이를 언제로 고쳐야 하는지 구하는 프로그램을 작성하시오.
 
 def alarm():
-    hour, minute = map(int, input().split())
-    if minute > 45:
+    hour, minute = map(int, input('알람시간을 입력해 주세요.').split())
+    if 45 < minute < 60:
         minute -= 45
-        print(hour,'시', minute,'분')
+        print('알람 예정 시간은', hour,'시', minute,'분 입니다.')
     else:
         hour -= 1
         minute -= 45
-        print(hour,'시', abs(minute),'분')
+        print('알람 예정 시간은', hour,'시', abs(minute),'분 입니다.')
 
 alarm()
+
 
 
 # 1.3 세 정수 a, b, c를 입력으로 받아 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오. -----------------------------
 
 def order():
-    aList = list(map(int, input().split()))
-    for i in range(0, len(aList)-1):
-        for k in range(i+1, len(aList)):
-            if aList[i] > aList[k]:
-                aList[i], aList[k] = aList[k], aList[i]
-    print(aList[1])
+    a, b, c = map(int, input().split())
+    txt = [a, b, c]
+    for i in range(0, len(txt)-1):  # list의 0 1 2 요소 가져오기
+        for k in range(i+1, len(txt)):
+            if txt[i] > txt[k]:
+                txt[i], txt[k] = txt[k], txt[i]
+    print(txt[1])
 
 order()
 
@@ -98,18 +100,35 @@ for a in range(1, 1000):    # a에 들어갈 수 있는 수를 모두 뽑는다.
 
 
 # 2.2 5이상 9이하의 홀수를 입력받아 다이아몬드 형태의 별을 출력하는 프로그램을 작성하시오.
+def diamond():
+    N = int(input('5 이상 9 이하의 홀수를 입력해 주세요.'))
 
-N = int(input())
+    if 5 <= N <= 9 and N % 2 == 1:
+        print('올바른 입력입니다.')
+        for top in range((N//2)+1):  # 다이아몬드 중앙포함 위쪽 만들기
+            for top_blank in range(top, 4):
+                print(' ', end='')
+            for top_star in range((top*2)+1):
+                print('*', end='')
+            print()
+        for bot in reversed(range(N//2)):  # 아래쪽 만들기
+            for bot_blank in range(bot, 4):
+                print(' ', end='')
+            for bot_star in range((bot*2)+1):
+                print('*', end='')
+            print()
+    else:
+        print('올바르지 않은 입력입니다. 다시 입력해 주세요.')
 
-if 5 <= N <= 9 and N % 2 == 1:
-    for i in range(N-3):
-        for top_b in range(i, 4):
-            print(' ', end='')
-        for top_s in range((i*2)+1):
-            print('*', end='')
-        print()
+diamond()
 
-while True:
-    for i in range(3):
-        print(i)
-    break
+#  i  빈칸수   *수
+#  0  4(0:4)    1 (i*2)+1
+#  1  3(1:4)    3 (i*2)+1
+#  2  2         5
+#  3  1         7
+
+#  2  2(2:4)    5
+#  1  3(1:4)    3
+#  0  4(0:4)    1
+
