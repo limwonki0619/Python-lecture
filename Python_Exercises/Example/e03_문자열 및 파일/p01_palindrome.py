a9cda797e70dbe77cea26543f582f63eae4bd890
Wonki Lim <51067315+limwonki0619@users.noreply.github.com>
2019-07-25 10:29:05 +0900

    Update p01_palindrome.py

--- Python_Exercises/Example/e03_문자열 및 파일/p01_palindrome.py
+++ Python_Exercises/Example/e03_문자열 및 파일/p01_palindrome.py
@@ -4,18 +4,14 @@
 # 두 자리 수를 곱해 만들 수 있는 대칭수 중 가장 큰 수는 9009 (= 91 × 99) 이다.
 # 세 자리 수를 곱해 만들 수 있는 가장 큰 대칭수는 얼마인가?
 
-def palindrome():
-    maxI = 0
-    maxK = 0
-    pal = []
-    for i in range(999, 101, -1):
-        for k in range(999, 101-i, -1):
-            value = i * k
-            if str(value) == str(value)[::-1]:
-                pal.append(value)
-                maxI = i
-                maxK = k
+maxNum = 0
+for i in range(100, 1000):
+    for k in range(i, 1000):
+        value = i * k
+        if str(value) == str(value)[::-1]:
+            if i*k > maxNum:
+                maxNum = i*k
+                maxA = i
+                maxB = k
 
-    print('세 자리 수 Palindrome 중 가장 큰 수는 : {0} x {1} = {2}'.format(maxI, maxK, max(pal)))
-
-palindrome()
+print('세 자리 수 Palindrome 중 (x <= y)이 만족하는 가장 큰 수는 : {0} x {1} = {2}'.format(maxA, maxB, maxNum))
